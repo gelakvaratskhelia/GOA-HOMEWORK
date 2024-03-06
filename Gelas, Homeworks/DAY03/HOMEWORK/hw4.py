5c08795fa5c7f90e516658a3815d78c5b41e1294
Gela_K <147756343+gelakvaratskhelia@users.noreply.github.com>
2024-03-07 00:30:32 +0400

    Add files via upload

---
+++ Gelas, Homeworks/DAY03/HOMEWORK/hw4.py
@@ -0,0 +1,5 @@
+First_Name = input("whats your first name?: ")
+Last_Name = input("whats your last name?: " )
+
+print(f"Your First Name is {First_Name}")
+print(f"Your Last Name is {Last_Name}")
---
+++ Gelas, Homeworks/DAY06/HOMEWORK/hw7.py
@@ -0,0 +1,31 @@
+# for i in range (0, 11, 2):
+#         print(i, end = '    ')
+# #2
+# i = 1
+# while i <= 40:
+#         print(i, end= ' ' )
+#         i += 1
+
+# total = 0
+# i = 0
+# while i <= 5:
+#     total += i
+#     i += 1
+
+# print(total)
+
+# cifri = sum(range(6))
+# print(cifri)
+
+# str = "იყო არაბეთს როსტევან"
+# for char in str:
+
+#     print(char)
+
+for _ in range(10):
+    print("მე ვარ გელა")
+
+'''
+_ არის კონვენცია პითონში, რომელიც მიუთითებს იმაზე, რომ მარყუჟის ცვლადი არ გამოიყენება მარყუჟის შიგნით (ეს არის გადაყრის ცვლადი).
+
+'''
