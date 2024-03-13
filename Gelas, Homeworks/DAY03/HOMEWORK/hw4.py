63c2fa8b85e8c3d773de8e1d8ac6862f4e370470
Gela_K <147756343+gelakvaratskhelia@users.noreply.github.com>
2024-03-13 15:43:00 +0400

    Delete Gelas, Homeworks directory

--- Gelas, Homeworks/DAY03/HOMEWORK/hw4.py
+++
@@ -1,5 +0,0 @@
-First_Name = input("whats your first name?: ")
-Last_Name = input("whats your last name?: " )
-
-print(f"Your First Name is {First_Name}")
-print(f"Your Last Name is {Last_Name}")
--- Gelas, Homeworks/DAY04/HOMEWORK/hw5.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Enter Your Number: "))
-num2 = int(input("Enter Your 2nd Number: "))
-
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
-print(num1 / num2)
